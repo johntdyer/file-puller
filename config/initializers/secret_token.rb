b5dc20cdb2f125400a8fe424d981b2caf3d90016
John Dyer <jdyer@tropo.com>
2014-02-25 17:11:41 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FilePuller::Application.config.secret_key_base = 'fe439a583fbcbd619165a2f7b41c4f5f0cd3663c705f0cbba2a2f71f46dab4178f9fbec37682f7ab587b45c23de234522346f200e6f79ba6b7998c7135c21eb9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FilePuller::Application.config.session_store :cookie_store, key: '_file-puller_session'
