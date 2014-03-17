8a869fe834f7f98d3ee9c404cd2804c031dc86bc
John Dyer <jdyer@tropo.com>
2014-03-17 16:41:43 -0400

    Match new es-query constructor

--- app/workers/log_fetcher.rb
+++ app/workers/log_fetcher.rb
@@ -9,40 +9,82 @@ class LogFetcher
   Dotenv.load("#{Rails.root}/.env")
 
   def perform(params={})
-
+    params.merge!('file_name' => params[:metadata].nil? ? "#{jid}.log" : "#{jid}-#{params[:metadata]}.log")
     # Set some variables
-    file_name = params[:metadata].nil? ? "#{jid}.log" : "#{jid}-#{params[:metadata]}.log"
 
-    logger.info "JID #{jid} - TID #{Thread.current.object_id.to_s(36)} - Querying #{params[:query]} and writing results to #{file_name}"
+    logger.info "JID #{jid} - TID #{Thread.current.object_id.to_s(36)} - Querying #{params[:query]} and writing results to #{params['file_name']}"
 
-    # Start Query
+    results = nil
     begin
-      es_conn = Esquery.new( params['query'], params['start_time'], params['end_time'], "public/requests/#{file_name}", {})
-      es_conn.query_finished # returns boolean, true when query is complete
+      logger.info "params - #{params}"
+      results = perform_query params
+    rescue Exception => e
+      logger.error "Error performing query -> #{e}"
+    end
+
+    begin
+
+      request = update_active_record params,results
+
+      url = "http://#{ENV['DOMAIN']}/requests/#{request.id}/download"
+
+      logger.info "JID #{jid} - TID #{Thread.current.object_id.to_s(36)} - Query finished, found #{results.num_results} results.  Result can be found here #{url}"
+      send_notification params['email'], "Log request #{jid} finished. - found #{results.num_results}} results.","Query finished, found #{results.num_results}} results.  Result can be found here #{url}"
+
     rescue Exception => e
       logger.error "Something bad happened -> #{e}"
     end
 
-    request = nil;
-    begin
-      request = Request.find(params['id'])
-      logger.info "Request.find(params['id']) => #{request}"
-      request.update(log: File.open("#{Rails.root}/public/requests/#{file_name}"),jid: jid, results: es_conn.num_results)
-      if request.save
-        logger.info "Updated record"
-      else
-        logger.error "Failed to update record"
-      end
-    rescue Exception => e
-      logger.error e
-    end
-    url = "http://#{ENV['DOMAIN']}/requests/#{request.id}/download"
 
-    logger.info "JID #{jid} - TID #{Thread.current.object_id.to_s(36)} - Query finished, found #{es_conn.num_results} results.  Result can be found here #{url}"
-    send_notification params['email'], "Log request #{jid} finished. - found #{es_conn.num_results}} results.","Query finished, found #{es_conn.num_results}} results.  Result can be found here #{url}"
   end
 
   private
+
+  def update_active_record(params,results)
+    request = Request.find(params['id'])
+    logger.info "Request.find(params['id']) => #{request.inspect}"
+
+    request.update({
+                     log: File.open("#{Rails.root}/public/requests/#{params['file_name']}"),
+                     jid: jid,
+                     results: results.num_results
+    })
+
+    if request.save
+      logger.info "Updated record"
+      return request
+    else
+      logger.error "Failed to update record"
+      raise "Failed to update record"
+
+    end
+
+  end
+
+
+  def perform_query(params={})
+    logger.info "#{jid} - Query Starting"
+
+    es_conf = {
+      query: params['query'],
+      start_date: params['start_time'],
+      end_date: params['end_time'],
+      output_file: "public/requests/#{params['file_name']}",
+      index_prefixes: [ "logstash-", "logstash-cdr-", "logstash-haproxy-" ]
+    }
+
+    es_conn = Esquery.new(es_conf)
+
+    loop do
+
+      logger.debug "#{jid} - Query running"
+      sleep 2
+      break if es_conn.query_finished
+    end
+
+    logger.info "#{jid} - Query finished"
+    es_conn
+  end
 
   def send_notification(to,subject,body)
 
@@ -66,24 +108,3 @@ class LogFetcher
     )
   end
 end
-
-
-require 'es-query'
-jid = "testing123"
-params = {
-  metadata: "testing",
-  start_time: "2014-03-14T00:00:00.000Z",
-  end_time: "2014-03-14T04:00:00.000Z",
-  query: 'tags:"sensu-status-warning"  AND NOT "ESHeap"'
-}
-
-file_name = "test.log"
-
-es_conn = Esquery.new( params[:query], params[:start_time], params[:end_time], "public/requests/#{file_name}", {})
-
-
-until es_conn.query_finished do
-    print "."
-  end
-
-  puts "done -> #{es_conn.num_results}"
